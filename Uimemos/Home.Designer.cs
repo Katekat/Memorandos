c8ed91f7c51279d44931dd3b33006b6165dd25a8
katekat <katherineflorez23@gmail.com>
2016-03-04 21:55:32 -0430

    l

--- Uimemos/Home.Designer.cs
+++ Uimemos/Home.Designer.cs
@@ -257,7 +257,6 @@
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
